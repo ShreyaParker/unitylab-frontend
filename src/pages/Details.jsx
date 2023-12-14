46d6272be5d63194962ae4fc0dfca2082a2f906f
Shreya Parkar <shreyaparker317@gmail.com>
2023-12-14 23:18:10 +0530

    Set up folder structure and installed required libraries

---
+++ src/pages/Details.jsx
@@ -0,0 +1,10 @@
+
+const Details = () => {
+    return (
+        <div>
+            detail
+        </div>
+    );
+};
+
+export default Details;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div className="bg-red-700 font-bold">
+            home
+        </div>
+    );
+};
+
+export default Home;
