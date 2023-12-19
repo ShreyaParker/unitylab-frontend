59ea3422edf444b5e6805e6bb74ac5dee3e8ab27
Shreya Parkar <shreyaparker317@gmail.com>
2023-12-19 21:27:44 +0530

    Created Details component with dynamic routing  based on the objectID params for post details, fetching data from API.

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,10 +1,33 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-const Details = () => {
+
+function Details() {
+    const { objectID } = useParams();
+    const [post, setPost] = useState(null);
+
+    useEffect(() => {
+
+        fetch(`http://hn.algolia.com/api/v1/items/${objectID}`)
+            .then((response) => response.json())
+            .then((data) => setPost(data));
+        console.log(post)
+    }, [objectID]);
+
+    if (!post) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <div>
-            detail
+            <div className="p-2 flex flex-col backdrop-blur bg-green-50 m-2 rounded-2xl">
+                <h2>{post.title}</h2>
+                <p> {post.points}</p>
+                <p className="ml-auto">- {post.author}</p>
+            </div>
+
         </div>
     );
-};
+}
 
 export default Details;
